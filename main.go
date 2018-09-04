db0ff3253c98982c8170ff5ff4d55f575c31750c
tauraamui <tauracatamui@gmail.com>
2018-09-05 00:20:22 +0100

    start to add sprites for testing

--- main.go
+++ main.go
@@ -2,22 +2,65 @@ package main
 
 import (
 	"fmt"
+	"image"
+	_ "image/png"
+	"os"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+const (
+	screenWidth  = 320
+	screenHeight = 240
+
+	frameOX     = 0
+	frameOY     = 32
+	frameWidth  = 32
+	frameHeight = 32
+	frameNum    = 8
+)
+
+var (
+	sprite *ebiten.Image
+)
+
+func init() {
+
+	imgFile, err := os.Open("./res/image.png")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer imgFile.Close()
+
+	img, _, err := image.Decode(imgFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	sprite, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+}
+
 func update(screen *ebiten.Image) error {
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
+	w, h := sprite.Size()
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(0.5, 0.5)
+	op.GeoM.Translate(float64((screenWidth/2)-(w/4)), float64((screenHeight/2)-(h/4)))
+
+	screen.DrawImage(sprite, op)
 
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f", ebiten.CurrentFPS()))
 	return nil
 }
 
 func main() {
-	if err := ebiten.Run(update, 320, 240, 2, "Berrybun Game"); err != nil {
+	if err := ebiten.Run(update, screenWidth, screenHeight, 2, "Berrybun Game"); err != nil {
 		panic(err)
 	}
 }
