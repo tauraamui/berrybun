5c143d690e195e0e9f12fe56f15538eeff9f99d4
tauraamui <tauracatamui@gmail.com>
2018-09-06 23:18:21 +0100

    add controller support

--- main.go
+++ main.go
@@ -4,26 +4,32 @@ import (
 	"fmt"
 	"image"
 	_ "image/png"
+	"log"
 	"os"
+	"strconv"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/inpututil"
 )
 
 const (
 	screenWidth  = 320
 	screenHeight = 240
 
-	frameOX     = 0
-	frameOY     = 32
+	frameOX = 0
+	// frameOY = 0
+	// frameOY     = 32
 	frameWidth  = 32
 	frameHeight = 32
 	frameNum    = 6
 )
 
 var (
-	count  = 0
-	sprite *ebiten.Image
+	count      = 0
+	sprite     *ebiten.Image
+	gamepadIDs = map[int]struct{}{}
+	frameOY    = 0
 )
 
 func init() {
@@ -46,15 +52,50 @@ func init() {
 }
 
 func update(screen *ebiten.Image) error {
+
+	// Log the gamepad connection events.
+	for _, id := range inpututil.JustConnectedGamepadIDs() {
+		log.Printf("gamepad connected: id: %d", id)
+		gamepadIDs[id] = struct{}{}
+	}
+	for id := range gamepadIDs {
+		if inpututil.IsGamepadJustDisconnected(id) {
+			log.Printf("gamepad disconnected: id: %d", id)
+			delete(gamepadIDs, id)
+		}
+	}
+
+	ids := ebiten.GamepadIDs()
+	axes := []float64{}
+	pressedButtons := map[int][]string{}
+
+	for _, id := range ids {
+		maxAxis := ebiten.GamepadAxisNum(id)
+		for a := 0; a < maxAxis; a++ {
+			v := ebiten.GamepadAxis(id, a)
+			axes = append(axes, v)
+		}
+		maxButton := ebiten.GamepadButton(ebiten.GamepadButtonNum(id))
+		for b := ebiten.GamepadButton(id); b < maxButton; b++ {
+			if ebiten.IsGamepadButtonPressed(id, b) {
+				pressedButtons[id] = append(pressedButtons[id], strconv.Itoa(int(b)))
+			}
+
+			// Log button events.
+			if inpututil.IsGamepadButtonJustPressed(id, b) {
+				log.Printf("button pressed: id: %d, button: %d", id, b)
+			}
+			if inpututil.IsGamepadButtonJustReleased(id, b) {
+				log.Printf("button released: id: %d, button: %d", id, b)
+			}
+		}
+	}
+
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
 
 	count++
-
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
 
 	op := &ebiten.DrawImageOptions{}
 	//move sprite backwards and up by half of its width and height
@@ -68,7 +109,23 @@ func update(screen *ebiten.Image) error {
 	op.SourceRect = &r
 	screen.DrawImage(sprite, op)
 
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f", ebiten.CurrentFPS()))
+	// Draw the current gamepad status.
+	str := ""
+	str += fmt.Sprintf("FPS: %0.2f\n", ebiten.CurrentFPS())
+
+	if axes[0] >= 0.5 {
+		frameOY = 32
+	} else {
+		frameOY = 0
+	}
+
+	if len(ids) > 0 {
+
+	} else {
+		str = "Please connect your gamepad."
+	}
+	ebitenutil.DebugPrint(screen, str)
+
 	return nil
 }
 
