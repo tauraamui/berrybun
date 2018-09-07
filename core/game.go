87180be09775224eaf51403d7855b9c068ef10fc
tauraamui <aplewis95@gmail.com>
2018-09-07 15:32:17 +0100

    restructure logic to handle multiple gamepads input

--- core/game.go
+++ core/game.go
@@ -4,64 +4,138 @@ import (
 	"image"
 	"log"
 	"os"
-	"strconv"
+	"sync"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/inpututil"
 )
 
+const (
+	screenWidth  = 320
+	screenHeight = 240
+)
+
+var count = 0
+
+type GamePadInput struct {
+	id   int
+	axes []float64
+}
+
+func (gpi *GamePadInput) update() {
+	for a := 0; a < len(gpi.axes); a++ {
+		v := ebiten.GamepadAxis(gpi.id, a)
+		gpi.axes[a] = v
+	}
+}
+
 type Game struct {
-	player     *Player
-	gamepadIDs map[int]struct{}
+	mu       sync.Mutex
+	player   *Player
+	gamepads []GamePadInput
 }
 
 func (g *Game) Init() {
 	g.player = &Player{
+		game: g,
 		animSpriteSheetLocation: "./res/bunny.png",
+		frameOX:                 0,
+		frameOY:                 0,
+		frameWidth:              32,
+		frameHeight:             32,
+		frameNum:                6,
+		animationSpeed:          8,
 	}
 	g.player.Init()
 }
 
-func (g *Game) Update(screen *ebiten.Image) error {
-	g.updateGamepad()
-	return nil
+//AddGamepad adds a gamepad struct to collection if doesn't already contain gamepad of same id
+func (g *Game) AddGamepad(gp GamePadInput) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+	gamepadAlreadyInList := false
+	for _, existingGamepad := range g.gamepads {
+		if existingGamepad.id == gp.id {
+			gamepadAlreadyInList = true
+			break
+		}
+	}
+	if !gamepadAlreadyInList {
+		g.gamepads = append(g.gamepads, gp)
+	}
 }
 
-func (g *Game) updateGamepad() {
-	// Log the gamepad connection events.
+//DeleteGamepad finds gamepad of provided id and then deletes from collection
+func (g *Game) DeleteGamepad(gpid int) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	deleteIndex := -1
+	for i := 0; i < len(g.gamepads); i++ {
+		if g.gamepads[i].id == gpid {
+			deleteIndex = i
+			break
+		}
+	}
+	if deleteIndex > 0 {
+		g.gamepads = append(g.gamepads[:deleteIndex], g.gamepads[:deleteIndex+1]...)
+	}
+}
+
+func (g *Game) updateGamepads() {
+	// check for any disconnected gamepads and remove from game
 	for _, id := range inpututil.JustConnectedGamepadIDs() {
 		log.Printf("gamepad connected: id: %d", id)
-		g.gamepadIDs[id] = struct{}{}
-	}
-	for id := range g.gamepadIDs {
-		if inpututil.IsGamepadJustDisconnected(id) {
-			log.Printf("gamepad disconnected: id: %d", id)
-			delete(g.gamepadIDs, id)
-		}
-	}
-
-	ids := ebiten.GamepadIDs()
-	axes := []float64{}
-	pressedButtons := map[int][]string{}
-
-	for _, id := range ids {
-		maxAxis := ebiten.GamepadAxisNum(id)
-		for a := 0; a < maxAxis; a++ {
-			v := ebiten.GamepadAxis(id, a)
-			axes = append(axes, v)
-		}
-		maxButton := ebiten.GamepadButton(ebiten.GamepadButtonNum(id))
-		for b := ebiten.GamepadButton(id); b < maxButton; b++ {
-			if ebiten.IsGamepadButtonPressed(id, b) {
-				pressedButtons[id] = append(pressedButtons[id], strconv.Itoa(int(b)))
+		gamepadAlreadyInList := false
+		for _, gp := range g.gamepads {
+			if gp.id == id {
+				gamepadAlreadyInList = true
+				break
 			}
 		}
+		if !gamepadAlreadyInList {
+			g.AddGamepad(GamePadInput{
+				id:   id,
+				axes: make([]float64, ebiten.GamepadAxisNum(id)),
+			})
+		}
 	}
+
+	// check for any connected gamepads and add them to the game
+	for i := 0; i < len(g.gamepads); i++ {
+		if inpututil.IsGamepadJustDisconnected(g.gamepads[i].id) {
+			log.Printf("gamepad disconnected: id: %d", g.gamepads[i].id)
+			g.DeleteGamepad(g.gamepads[i].id)
+		}
+	}
+
+	for i := 0; i < len(g.gamepads); i++ {
+		g.gamepads[i].update()
+	}
+}
+
+//Update updates everything within game state
+func (g *Game) Update(screen *ebiten.Image) error {
+	g.updateGamepads()
+
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	return g.player.Update(screen)
+
 }
 
 type Player struct {
+	game                    *Game
 	animSpriteSheetLocation string
 	animSpriteSheet         *ebiten.Image
+	frameOX                 int
+	frameOY                 int
+	frameWidth              int
+	frameHeight             int
+	frameNum                int
+	animationSpeed          int
 }
 
 func (p *Player) Init() {
@@ -80,4 +154,35 @@ func (p *Player) Init() {
 	}
 
 	p.animSpriteSheet, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
+}
+
+func (p *Player) Update(screen *ebiten.Image) error {
+	count++
+
+	op := &ebiten.DrawImageOptions{}
+	//move sprite backwards and up by half of its width and height
+	op.GeoM.Translate(-float64(p.frameWidth)/2, -float64(p.frameHeight)/2)
+	//move sprite's origin to half of screen in width and height
+	op.GeoM.Translate(screenWidth/2, screenHeight/2)
+	//speed of changing from one animation frame to another
+	i := (count / p.animationSpeed) % p.frameNum
+	sx, sy := p.frameOX+i*p.frameWidth, p.frameOY
+	r := image.Rect(sx, sy, sx+p.frameWidth, sy+p.frameHeight)
+	op.SourceRect = &r
+	err := screen.DrawImage(p.animSpriteSheet, op)
+
+	if err != nil {
+		return err
+	}
+
+	if len(p.game.gamepads) > 0 {
+		firstGamepad := p.game.gamepads[0]
+		if firstGamepad.axes[0] >= 0.5 {
+			p.frameOY = 32
+		} else {
+			p.frameOY = 0
+		}
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,145 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"image"
 	_ "image/png"
-	"log"
-	"os"
-	"strconv"
 
 	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
-	"github.com/hajimehoshi/ebiten/inpututil"
 	"github.com/tauraamui/berrybun/core"
 )
 
 const (
 	screenWidth  = 320
 	screenHeight = 240
-
-	frameOX = 0
-	// frameOY = 0
-	// frameOY     = 32
-	frameWidth  = 32
-	frameHeight = 32
-	frameNum    = 6
 )
-
-var (
-	count          = 0
-	sprite         *ebiten.Image
-	gamepadIDs     = map[int]struct{}{}
-	frameOY        = 0
-	animationSpeed = 8
-)
-
-func init() {
-
-	imgFile, err := os.Open("./res/bunny.png")
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer imgFile.Close()
-
-	img, _, err := image.Decode(imgFile)
-
-	if err != nil {
-		panic(err)
-	}
-
-	sprite, _ = ebiten.NewImageFromImage(img, ebiten.FilterDefault)
-}
-
-func update(screen *ebiten.Image) error {
-
-	// Log the gamepad connection events.
-	for _, id := range inpututil.JustConnectedGamepadIDs() {
-		log.Printf("gamepad connected: id: %d", id)
-		gamepadIDs[id] = struct{}{}
-	}
-	for id := range gamepadIDs {
-		if inpututil.IsGamepadJustDisconnected(id) {
-			log.Printf("gamepad disconnected: id: %d", id)
-			delete(gamepadIDs, id)
-		}
-	}
-
-	ids := ebiten.GamepadIDs()
-	axes := []float64{}
-	pressedButtons := map[int][]string{}
-
-	for _, id := range ids {
-		maxAxis := ebiten.GamepadAxisNum(id)
-		for a := 0; a < maxAxis; a++ {
-			v := ebiten.GamepadAxis(id, a)
-			axes = append(axes, v)
-		}
-		maxButton := ebiten.GamepadButton(ebiten.GamepadButtonNum(id))
-		for b := ebiten.GamepadButton(id); b < maxButton; b++ {
-			if ebiten.IsGamepadButtonPressed(id, b) {
-				pressedButtons[id] = append(pressedButtons[id], strconv.Itoa(int(b)))
-			}
-
-			// Log button events.
-			if inpututil.IsGamepadButtonJustPressed(id, b) {
-				log.Printf("button pressed: id: %d, button: %d", id, b)
-			}
-			if inpututil.IsGamepadButtonJustReleased(id, b) {
-				log.Printf("button released: id: %d, button: %d", id, b)
-			}
-		}
-	}
-
-	if ebiten.IsDrawingSkipped() {
-		return nil
-	}
-
-	count++
-
-	op := &ebiten.DrawImageOptions{}
-	//move sprite backwards and up by half of its width and height
-	op.GeoM.Translate(-float64(frameWidth)/2, -float64(frameHeight)/2)
-	//move sprite's origin to half of screen in width and height
-	op.GeoM.Translate(screenWidth/2, screenHeight/2)
-	//speed of changing from one animation frame to another
-	i := (count / animationSpeed) % frameNum
-	sx, sy := frameOX+i*frameWidth, frameOY
-	r := image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)
-	op.SourceRect = &r
-	screen.DrawImage(sprite, op)
-
-	// Draw the current gamepad status.
-	str := ""
-	str += fmt.Sprintf("FPS: %0.2f\n", ebiten.CurrentFPS())
-
-	if axes[0] >= 0.5 {
-		frameOY = 32
-		if axes[0] >= 0.9 {
-			if animationSpeed > 5 {
-				animationSpeed--
-			} else {
-				animationSpeed = 5
-			}
-		} else {
-			animationSpeed = 8
-		}
-	} else {
-		frameOY = 0
-		animationSpeed = 8
-	}
-
-	if len(ids) > 0 {
-
-	} else {
-		str = "Please connect your gamepad."
-	}
-	ebitenutil.DebugPrint(screen, str)
-
-	return nil
-}
 
 func main() {
 	var game = core.Game{}
