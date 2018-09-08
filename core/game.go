7b137dc7152ce7a48e1c05af844c456bb125c2c1
tauraamui <tauracatamui@gmail.com>
2018-09-08 12:24:29 +0100

    nieve implementation of resolution variable sprite scaling for animations

--- core/game.go
+++ core/game.go
@@ -152,14 +152,14 @@ func (p *Player) Init() {
 		spritesheet:        animSpriteSheet,
 		repeatLoopStart:    0,
 		repeatLoopEnd:      1,
-		maxRepeatLoopCount: 260,
+		maxRepeatLoopCount: 200,
 		frameWidth:         32,
 		frameHeight:        32,
 		frame0X:            0,
 		frame0Y:            0,
 		frameNum:           6,
-		defaultSpeed:       4,
-		speed:              4,
+		defaultSpeed:       2,
+		speed:              2,
 		count:              -1,
 	}
 
@@ -242,7 +242,6 @@ func (p *Player) Update(screen *ebiten.Image) error {
 
 type Animation struct {
 	id                 uint
-	defaultSpeed       int
 	spritesheet        *ebiten.Image
 	repeatLoopStart    int
 	repeatLoopEnd      int
@@ -253,6 +252,7 @@ type Animation struct {
 	frame0X            int
 	frame0Y            int
 	frameNum           int
+	defaultSpeed       int
 	speed              int
 	count              int
 	countForLoopStart  int
@@ -266,24 +266,38 @@ func (a *Animation) Update(screen *ebiten.Image) error {
 
 	a.count++
 
+	scale := ebiten.DeviceScaleFactor()
+
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(-float64(a.frameWidth)/2, -float64(a.frameHeight)/2)
-	op.GeoM.Translate(screenWidth/2, screenHeight/2)
+	sw, sh := screen.Size()
+	swf := float64(sw) - (float64(sw) * float64(0.9988))
+	shf := float64(sh) - (float64(sh) * float64(0.9988))
+	op.GeoM.Scale(scale+swf, scale+shf)
+	op.GeoM.Translate(float64(sw)/2, float64(sh)/2)
 	i := (a.count / a.speed) % a.frameNum
 
+	//if current frame is now on start loop sprite
 	if i == a.repeatLoopStart {
+		//get the count value for this position
 		a.countForLoopStart = a.count
 	}
 
+	//if current frame is now at end of loop
 	if i == a.repeatLoopEnd {
+		//if more loops to do
 		if a.repeatLoopCount < a.maxRepeatLoopCount {
+			//set animation back to the start of the loop
 			a.count = a.countForLoopStart
 			a.repeatLoopCount++
 		}
 	}
 
+	//if maximum number of loops occurred
 	if a.repeatLoopCount >= a.maxRepeatLoopCount {
+		//if current frame is end of animation
 		if i == a.frameNum-1 {
+			//set the loop count back
 			a.repeatLoopCount = 0
 		}
 	}
--- main.go
+++ main.go
@@ -7,16 +7,26 @@ import (
 	"github.com/tauraamui/berrybun/core"
 )
 
-const (
-	screenWidth  = 320
-	screenHeight = 240
-)
-
 func main() {
 	var game = core.Game{}
 	game.Init()
 
-	if err := ebiten.Run(game.Update, screenWidth, screenHeight, 2, "Berrybun Game"); err != nil {
+	//ebiten.SetFullscreen(true)
+
+	w, h := ebiten.MonitorSize()
+	// On mobiles, ebiten.MonitorSize is not available so far.
+	// Use arbitrary values.
+	if w == 0 || h == 0 {
+		w = 300
+		h = 450
+	}
+
+	ebiten.SetFullscreen(true)
+
+	s := ebiten.DeviceScaleFactor()
+
+	if err := ebiten.Run(game.Update, int(float64(w)*s), int(float64(h)*s), 1/s, "Berrybun Game"); err != nil {
+		// if err := ebiten.Run(game.Update, 800, 600, 1/s, "Berrybun Game"); err != nil {
 		panic(err)
 	}
 }
