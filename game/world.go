8ea2bc0e3be9d1b3879b0941fe7801f9854635b8
tauraamui <aplewis95@gmail.com>
2018-09-11 13:06:51 +0100

    start to add facility of rending world/map sprites

--- game/world.go
+++ game/world.go
@@ -6,14 +6,28 @@ import (
 
 type World struct {
 	game   *Game
+	wMap   *Map
 	player *Player
 }
 
 func (w *World) Init() {
+	w.wMap = &Map{
+		world:   w,
+		bglayer: []uint32{23, 23},
+	}
 	w.player.Init()
 }
 
 func (w *World) Update(screen *ebiten.Image) error {
 	w.player.Update(screen)
+	return nil
+}
+
+type Map struct {
+	world   *World
+	bglayer []uint32
+}
+
+func (m *Map) Update(screen *ebiten.Image) error {
 	return nil
 }
